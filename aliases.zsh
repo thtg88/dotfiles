b883f74fbedaa90dbb3f0809f4eb458954d95b77
Marco Marassi <thtg88@users.noreply.github.com>
2021-09-08 15:19:15 +0100

    Commented out MSSQL alias

--- aliases.zsh
+++ aliases.zsh
@@ -25,8 +25,9 @@ alias watch="npm run watch"
 # Docker
 alias docker-composer="docker-compose"
 
+# Not using MSSQL, commenting out for now
 # SQL Server
-alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
+# alias mssql="docker run -e ACCEPT_EULA=Y -e SA_PASSWORD=LaravelWow1986! -p 1433:1433 mcr.microsoft.com/mssql/server:2017-latest"
 
 # Git
 alias gst="git status"
