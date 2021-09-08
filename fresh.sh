c841329153e319ddb1f6eec32b8fe59e2f3d615b
Marco Marassi <thtg88@users.noreply.github.com>
2021-09-08 15:20:18 +0100

    Commented out MySQL setup in `fresh` script as not using at the moment

--- fresh.sh
+++ fresh.sh
@@ -20,8 +20,9 @@ brew update
 brew tap homebrew/bundle
 brew bundle
 
+#  Not using MySQL at the moment - commenting out
 # Set default MySQL root password and auth type
-mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+# mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
 
 # TODO: Postgres setup
 
