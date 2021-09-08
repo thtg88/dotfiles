dc61baaa568e8d35b44aa163ac71cb61ed60e9e0
Marco Marassi <thtg88@users.noreply.github.com>
2021-09-08 15:09:59 +0100

    Removed Oh My Zsh and mackup setup from `fresh` script

--- fresh.sh
+++ fresh.sh
@@ -2,10 +2,11 @@
 
 echo "Setting up your Mac..."
 
+# I'm not using Oh My Zsh - commenting this out.
 # Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
-fi
+# if test ! $(which omz); then
+#   /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+# fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
@@ -45,9 +46,6 @@ mkdir $HOME/Sites/laravel
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences - we will run this last because this will reload the shell
 source .macos
